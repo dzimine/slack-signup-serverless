830da3aaddb3bcd0e617900d989aad75d955f390
Dmitri Zimine <dz@stackstorm.com>
2017-06-05 08:16:24 -0700

    invite_slack implementation and unit tests.

--- invite_slack/handler.py
+++ invite_slack/handler.py
@@ -1,17 +1,83 @@
+#!/usr/bin/python
+import sys
+import os
+
+# Import installed packages (in site-packages)
+site_pkgs = os.path.join(os.path.dirname(os.path.realpath(__file__)), "site-packages")
+sys.path.append(site_pkgs)
+
+import urllib
 import json
-import datetime
+import requests
 
 
 def endpoint(event, context):
-    current_time = datetime.datetime.now().time()
-    body = {
-        "handler": "invite_slack",
-        "message": "Invite slack " + str(current_time)
-    }
+    print("Received event: " + event)
 
-    response = {
+    try:
+        event = json.loads(event)
+        TOKEN = os.environ['SLACK_TOKEN']
+        CHANNEL_IDS = os.environ.get('CHANNEL_IDS', '')
+        TEAM = os.environ['SLACK_TEAM']
+    except KeyError:
+        print "ERROR: Environment variables SLACK_TOKEN and SLACK_TEAM must be set."
+        return {
+            "statusCode": 500,
+            "body": "Invite not sent: SLACK_TOKEN and SLACK_TEAM must be set"
+            }
+    except ValueError as e:
+        print "ERROR: Can not parse `event` input: " + str(e)
+        return {
+            "statusCode": 400,
+            "body": "Invite not sent: bad input"
+        }
+
+    invite(event.get('email'),
+           token=TOKEN,
+           team=TEAM,
+           first_name=event.get('first_name'),
+           last_name=event.get('last_name', ''),
+           channel_ids=CHANNEL_IDS)
+    return {
         "statusCode": 200,
-        "body": json.dumps(body)
-    }
+        "body": "Invite sent"
+        }
 
-    return response
+
+def invite(email, token, team, first_name, last_name,
+           channel_ids=[], set_active=True, resend=True):
+    """
+        Invite a new user to slack organization.
+
+        Part of Slack undocumented API, documented here:
+        https://github.com/ErikKalkoken/slackApiDoc/blob/master/users.admin.invite.md
+    """
+
+    url = "https://%s.slack.com/api/users.admin.invite" % team
+    headers = {}
+    headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    body = {
+        'email': email.encode('utf-8'),
+        'channels': ",".join(channel_ids),
+        'token': token,
+        'set_active': set_active,
+        'resend': resend,
+        '_attempts': 1
+    }
+    if first_name is not None:
+        body['first_name'] = first_name.encode('utf-8')
+
+    data = urllib.urlencode(body)
+    response = requests.get(url=url,
+                            headers=headers, params=data)
+    results = response.json()
+
+    if results['ok'] is True:
+        print "Invite successfully sent to %s. RESPONSE: %s" % \
+            (email, results)
+    else:
+        failure_reason = ('Failed to send invite to %s: %s \
+                          (status code: %s)' % (email, response.text,
+                          response.status_code))
+        print failure_reason
+        raise Exception(failure_reason)
